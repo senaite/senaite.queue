0297babcd285636317cda5f7c69b0dad782b0811
Jordi Puiggené <jp@naralabs.com>
2020-10-26 00:48:09 +0100

    Added viewlet for queue server status

--- src/senaite/queue/browser/viewlets/__init__.py
+++ src/senaite/queue/browser/viewlets/__init__.py
@@ -19,10 +19,13 @@
 # Some rights reserved, see README and LICENSE.
 
 import itertools
-
+import requests
+import time
 from plone.app.layout.viewlets import ViewletBase
+from plone.memoize import ram
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from senaite.queue import api
+from senaite.queue import is_installed
 
 
 class QueuedAnalysesViewlet(ViewletBase):
@@ -71,3 +74,42 @@ class QueuedAnalysesSampleViewlet(ViewletBase):
         analyses = self.context.getAnalyses()
         queued = filter(api.is_queued, analyses)
         return len(queued)
+
+
+class QueueServerStatusViewlet(ViewletBase):
+    """Prints a viewlet to display a message when the status of the queue
+    server is not suitable
+    """
+    index = ViewPageTemplateFile("templates/queue_server_status.pt")
+
+    def __init__(self, context, request, view, manager=None):
+        super(QueueServerStatusViewlet, self).__init__(
+            context, request, view, manager=manager)
+        self.context = context
+        self.request = request
+        self.view = view
+
+    @ram.cache(lambda *args: time.time() // 60)
+    def get_server_status(self):
+        """Returns the current status of the queue server
+        """
+        if not is_installed():
+            return "ok"
+
+        if api.is_queue_server():
+            return "ok"
+
+        server_url = api.get_server_url()
+        if not server_url:
+            # Server url is not valid
+            return "invalid"
+
+        # Ping
+        url = "{}/@@API/senaite/v1/version".format(server_url)
+        try:
+            # Check the request was successful. Raise exception otherwise
+            r = requests.get(url, timeout=1)
+            r.raise_for_status()
+            return "ok"
+        except:  # noqa don't care about the response, want a ping only
+            return "timeout"
