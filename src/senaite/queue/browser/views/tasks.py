371479c2a79401249232a9235453eeadf104703f
Jordi Puiggené <jp@naralabs.com>
2020-10-26 11:05:11 +0100

    Missed to add the view and the template

---
+++ src/senaite/queue/browser/views/tasks.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.QUEUE.
+#
+# SENAITE.QUEUE is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2019-2020 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+from operator import itemgetter
+
+import collections
+from datetime import datetime
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from senaite.queue import api as qapi
+from senaite.queue import messageFactory as _
+
+from bika.lims import api
+from bika.lims.browser.bika_listing import BikaListingView
+from bika.lims.utils import get_link
+
+
+class TasksView(BikaListingView):
+
+    template = ViewPageTemplateFile("templates/tasks.pt")
+
+    def __init__(self, context, request):
+        super(TasksView, self).__init__(context, request)
+
+        # Query is ignored in `folderitems` method and only there to override
+        # the default settings
+        self.catalog = "uid_catalog"
+        self.contentFilter = {"UID": api.get_uid(context)}
+
+        # Set the view name with `@@` prefix to get the right API URL
+        self.__name__ = "@@queue_tasks"
+
+        self.show_select_column = False
+        self.show_search = False
+
+        self.show_categories = False
+        self.expand_all_categories = True
+        self.categories = []
+
+        self.title = _("Queue monitor")
+        self.sort_on = "priority"
+        self.sort_order = "ascending"
+
+        self.columns = collections.OrderedDict((
+            ("task_short_uid", {
+                "title": _("Task UID"),
+                "sortable": False,
+            }),
+            ("priority", {
+                "title": _("Priority"),
+                "sortable": True,
+            }),
+            ("created", {
+                "title": _("Created"),
+                "sortable": True,
+            }),
+            ("name", {
+                "title": _("Name"),
+                "sortable": True,
+            }),
+            ("context_path", {
+                "title": _("Context"),
+                "sortable": True,
+            }),
+            ("username", {
+                "title": _("Username"),
+                "sortable": True,
+            }),
+            ("status", {
+                "title": _("Status"),
+                "sortable": True,
+            })
+        ))
+
+        self.review_states = [
+            {
+                "id": "default",
+                "title": _("All tasks"),
+                "contentFilter": {},
+                "columns": self.columns.keys(),
+            },
+        ]
+
+    def update(self):
+        """Update hook
+        """
+        self.request.set("disable_border", 1)
+        self.request.set("disable_plone.rightcolumn", 1)
+        super(TasksView, self).update()
+
+    def folderitems(self):
+        # flag for manual sorting
+        self.manual_sort_on = self.get_sort_on()
+
+        # Get the items
+        items = map(self.make_item, qapi.get_queue().get_tasks())
+
+        # Infere the priorities
+        site_url = api.get_url(api.get_portal())
+        api_url = "{}/@@API/v1/@@API/senaite/v1/queue_server".format(site_url)
+        idx = 1
+        for item in items:
+            if item["status"] not in ["queued", "running"]:
+                priority = 0
+            else:
+                priority = idx
+                idx += 1
+
+            created = datetime.fromtimestamp(int(item["created"])).isoformat()
+            context_link = get_link(item["context_path"], item["context_path"])
+
+            task_link = "{}/{}".format(api_url, item["uid"])
+            params = {"class": "text-monospace"}
+            task_link = get_link(task_link, item["task_short_uid"], **params)
+            item.update(
+                {"priority": str(priority).zfill(4),
+                 "replace": {
+                     "status": _(item["status"]),
+                     "context_path": context_link,
+                     "task_short_uid": task_link,
+                     "created": created,
+                 }}
+            )
+
+        # Sort the items
+        sort_on = self.manual_sort_on in self.columns.keys() or "priority"
+        reverse = self.get_sort_order() == "ascending"
+        items = sorted(items, key=itemgetter(sort_on), reverse=reverse)
+
+        return items
+
+    def make_empty_item(self, **kw):
+        """Creates a new empty item
+        """
+        item = {
+            "uid": None,
+            "before": {},
+            "after": {},
+            "replace": {},
+            "allow_edit": [],
+            "disabled": False,
+            "state_class": "state-active",
+        }
+        item.update(**kw)
+        return item
+
+    def make_item(self, task):
+        item = self.make_empty_item()
+        item.update({
+            "uid": task.task_uid,
+            "task_short_uid": task.task_short_uid,
+            "priority": task.priority,
+            "created": task.created,
+            "name": task.name,
+            "context_path": task.context_path,
+            "username": task.username,
+            "status": task.status,
+        })
+        return item
